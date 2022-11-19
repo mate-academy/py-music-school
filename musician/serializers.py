f7631b9ec47352160e991ecdead920a7737606f2
Roman Kolmakov <cerne313@gmail.com>
2022-11-19 18:38:11 +0200

    Solve the task

---
+++ musician/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(min_value=14)
+
+    class Meta:
+        model = Musician
+        fields = (
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "is_adult",
+            "date_of_applying"
+        )
+
