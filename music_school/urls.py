6e983f5e08f777cb88feeadb5dd2da7af23116e9
stoveprofi <stoveprofi@gmail.com>
2022-11-24 12:30:57 +0000

    Corrected flake8

--- music_school/urls.py
+++ music_school/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/musician/", include(
-        "musician.urls", namespace="musician")
-         ),
+    path(
+        "api/musician/",
+        include("musician.urls", namespace="musician")
+    ),
 ]
