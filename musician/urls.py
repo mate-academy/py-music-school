dd370f97fef62fd84bb6f8f2c03eeaa58939efeb
Volodymyr Panov <vpanov25@gmail.com>
2022-12-25 21:37:01 +0100

    Solution

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = []
+from django.urls import path
+
+from musician.views import MusicianList, MusicianDetail
+
+urlpatterns = [
+    path("manage-list/", MusicianList.as_view(), name="manage-list"),
+    path(
+        "manage-list/<int:pk>/",
+        MusicianDetail.as_view(),
+        name="musician-detail"
+    ),
+]
 
 app_name = "musician"
---
+++ musician/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from musician.models import Musician
+from musician.serializers import MusicianSerializer
+
+
+class MusicianList(APIView):
+    def get(self, request):
+        genres = Musician.objects.all()
+        serializer = MusicianSerializer(genres, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MusicianSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MusicianDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Musician, pk=pk)
+
+    def get(self, request, pk):
+        genres = self.get_object(pk)
+        serializer = MusicianSerializer(genres)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        genres = self.get_object(pk)
+        serializer = MusicianSerializer(genres, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        genres = self.get_object(pk)
+        serializer = MusicianSerializer(
+            genres, data=request.data, partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        genres = self.get_object(pk)
+        genres.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
