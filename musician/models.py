61b0d8818f05a34cc030e6e2919c7f684f27d85c
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-12 10:24:08 +0100

    Added model Musician

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,23 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField(
+        validators=[MinValueValidator(
+            14, message="We do not accept people who are under 14"
+        )]
+    )
+    date_of_applying = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_adult(self) -> bool:
+        if self.age >= 21:
+            return True
+        return False
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
