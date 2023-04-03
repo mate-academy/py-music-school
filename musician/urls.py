5037ab08a52fb4b131761086878558eefa351c72
manjustice <manjustice1998@gmail.com>
2023-04-03 18:28:46 +0300

    Solution

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from musician.view import MusicianViewSet
+
+routers = routers.DefaultRouter()
+routers.register("musicians", MusicianViewSet, basename="manage")
+
+urlpatterns = [
+    path("", include(routers.urls))
+]
 
 app_name = "musician"
