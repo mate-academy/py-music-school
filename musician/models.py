db8d9eef64dae0eaaf7d7b1d7cee3aaf5be58e0e
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-04-05 22:40:52 +0300

    Solution

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,26 @@
+from typing import Type
+
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField()
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def is_adult(self) -> bool:
+        return 21 >= self.age
+
+    def age_clean(self) -> None:
+        if self.age < 14:
+            raise ValidationError("We do not accept people who are under 14")
+
+    def save(self, *args, **kwargs) -> Type["Musician"]:
+        self.full_clean()
+        return super().save(*args, **kwargs)
---
+++ musician/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        )
+
+
+class MusicianCreateSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(min_value=14)
+
+    class Meta:
+        model = Musician
+        fields = ("id", "first_name", "last_name", "instrument", "age")
