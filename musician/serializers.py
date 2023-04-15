298b150394411b5a7a523be1d5ade713d2494cf4
Andriy Demeshko <ande777complaint@gmail.com>
2023-04-15 12:12:28 +0300

    Solution final

--- musician/serializers.py
+++ musician/serializers.py
@@ -4,6 +4,8 @@ from musician.models import Musician
 
 
 class MusicianSerializer(serializers.ModelSerializer):
+    is_adult = serializers.ReadOnlyField()
+
     class Meta:
         model = Musician
-        fields = ("id", "first_name", "last_name", "instrument", "age", "date_of_applying")
+        fields = ("id", "first_name", "last_name", "instrument", "age", "date_of_applying", "is_adult")
