280aa4aca1680d98414912a0c8f86686a4f320f1
ZaikaDaria <corplinebusiness@gmail.com>
2023-07-11 00:08:41 +0300

    Add solution

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = []
+from rest_framework import routers
+
+from .views import MusicianViewSet
+
+router = routers.DefaultRouter()
+router.register("musicians", MusicianViewSet, basename="manage")
+
+urlpatterns = [
+
+              ] + router.urls
 
 app_name = "musician"
