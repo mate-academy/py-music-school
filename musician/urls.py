f3c89544436704ed247bdff9cb14d31e79224d36
Mariia <gorbova21@gmail.com>
2023-08-01 18:47:06 +0300

    implemented music school

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+
+from musician.views import MusicianViewSet
+
+router = DefaultRouter()
+router.register("musicians", MusicianViewSet, basename="manage")
+urlpatterns = router.urls
 
 app_name = "musician"
