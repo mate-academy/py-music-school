cd0672536f883f3cfbc6e6a2a3202c8fc95fcf7f
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-08-15 18:36:08 +0300

    Solution 2

--- musician/models.py
+++ musician/models.py
@@ -9,8 +9,9 @@ class Musician(models.Model):
     age = models.IntegerField(validators=[MinValueValidator(14)])
     date_of_applying = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
+    @property
     def is_adult(self):
         return self.age >= 21
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
