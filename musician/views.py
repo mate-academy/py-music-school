bcc0f4892919f2717c3feba0cb4fd8924ba62e44
Illia <kononenko2109@gmail.com>
2023-10-11 20:14:30 +0300

    solution

---
+++ musician/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status, generics, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from musician.models import Musician
+from musician.serializers import MusicianSerializer
+
+
+class MusicianViewSet(viewsets.ModelViewSet):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
