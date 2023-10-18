718a3d23c8c48012715a118c302632be72ac5e3f
VladyslavOladko <vladguitar03@gmail.com>
2023-10-18 22:03:18 +0300

    solution

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
+
+
+def validate_minimal_years(value):
+    if value < 14:
+        raise ValidationError(
+            "You are so young, come when you`re 14"
+        )
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField(validators=[validate_minimal_years])
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def is_adult(self):
+        if self.age >= 21:
+            return True
+        return False
--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = []
+from rest_framework import routers
+
+from musician.views import MusicianViewSet
+
+
+router = routers.DefaultRouter()
+router.register("musicians", MusicianViewSet)
+
+urlpatterns = [] + router.urls
 
 app_name = "musician"
