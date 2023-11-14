eb4bd90f38e4f50706513b94f8c6047b45972787
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-14 08:37:55 +0200

    Use f-string in def __str__ class Musician

--- musician/models.py
+++ musician/models.py
@@ -14,4 +14,4 @@ class Musician(models.Model):
         return self.age > 21
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
