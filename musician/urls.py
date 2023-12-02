8a3c2b8cf23f01ef55cca1cd4dddf27d853e6b70
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-12-02 12:22:06 +0200

    Connect [MusicianViewSet] to the [musician.urls]

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+
+from musician.views import MusicianViewSet
+
+router = DefaultRouter()
+router.register("musicians", MusicianViewSet, "manage")
+
+urlpatterns = router.urls
 
 app_name = "musician"
