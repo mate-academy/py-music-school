9a2bfc68270918fab23df12dd8e0faa6b9dabc33
Katherine-Greg <katiasimpson17@gmail.com>
2023-12-11 08:03:40 +0200

    Solution

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-
+from django.core import validators
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField(validators=[validators.MinValueValidator(14)])
+    date_of_applying = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_adult(self) -> bool:
+        return True if self.age > 20 else False
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
