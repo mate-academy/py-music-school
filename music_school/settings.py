ce4ddf274d5393c71f29ed4c6e2f6cd66a02b15f
Lylak Ivan <lylakivanmate@gmail.com>
2024-01-04 20:26:59 +0200

    Solution

--- music_school/settings.py
+++ music_school/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = "music_school.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "test_db.sqlite3",
+    },
 }
 
 
