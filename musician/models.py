a8a86e24268bc74833a69057b27066a76948befe
nsdNite <nsd7night@gmail.com>
2024-01-08 11:50:44 +0200

    Add age validator to model

--- musician/models.py
+++ musician/models.py
@@ -1,11 +1,17 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def validate_age(value):
+    if value <= 14:
+        raise ValidationError("Age must be over 14.")
 
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
     instrument = models.CharField(max_length=63)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[validate_age])
     date_of_applying = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
