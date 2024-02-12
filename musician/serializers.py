30a0db51763f995574ed4e87273024b5ab0cf643
sasha <magazynuniversam@gmail.com>
2024-02-12 21:37:22 +0200

    solution 3

--- musician/serializers.py
+++ musician/serializers.py
@@ -4,11 +4,13 @@ from musician.models import Musician
 
 
 class MusicianSerializer(serializers.ModelSerializer):
+    is_adult = serializers.CharField(read_only=True)
+
     class Meta:
         model = Musician
-        fields = ("id",
-                  "first_name",
+        fields = ("first_name",
                   "last_name",
-                  "age",
                   "instrument",
-                  "is_adult")
+                  "age",
+                  "date_of_applying",
+                  "is_adult",)
--- musician/urls.py
+++ musician/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from musician.views import MusicianViewSets
 
 routers = routers.DefaultRouter()
-routers.register("musicians", MusicianViewSets, basename="manage")
+routers.register("manage-list", MusicianViewSets, basename="manage")
 
 urlpatterns = routers.urls
 
