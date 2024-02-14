294b5e3f8673678b1b819ccda75f0b3628e387ad
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-02-14 17:06:15 +0200

    solution

---
+++ musician/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+
+from musician.models import Musician
+
+
+class MusicianSerializer(ModelSerializer):
+    class Meta:
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        )
