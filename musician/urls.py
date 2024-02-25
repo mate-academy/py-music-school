eaa11ed26f8c79076932e3a7c550fbc4ec7f46f4
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-02-26 00:37:38 +0200

    Solution

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,22 @@
-urlpatterns = []
+from django.urls import path
+from .views import MusicianViewSet
+
+
+urlpatterns = [
+    path("musicians/", MusicianViewSet.as_view(
+        actions={
+            "get": "list",
+            "post": "create",
+        }
+    ), name="manage-list"),
+    path("musicians/<int:pk>/", MusicianViewSet.as_view(
+        actions={
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        }
+    ), name="manage-item"),
+]
 
 app_name = "musician"
