991020ce7740f9e941c4678c9a24f087425443b3
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-03-28 15:43:38 +0200

    Implemented py-music-school task

---
+++ musician/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(min_value=14)
+
+    class Meta:
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult",
+        )
--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from musician.views import MusicianSetView
 
 app_name = "musician"
+
+router = routers.DefaultRouter()
+router.register("musicians", MusicianSetView, basename="manage")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
